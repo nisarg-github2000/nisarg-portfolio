cbf81f5702bcb085340fcde3a040fe436db2c208
nisarg-github2000 <nisargoza2000@gmail.com>
2024-10-29 04:41:52 -0700

    Minor Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,21 +16,21 @@ document.addEventListener("DOMContentLoaded", function() {
         loop: true
     });
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        items:2,
-        margin:30,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        responsive:{
-            0:{
-                items:1
-            },
-            900:{
-                items:2
-            },
-        }
-    })
+    // $('.owl-carousel').owlCarousel({
+    //     loop:true,
+    //     items:2,
+    //     margin:30,
+    //     autoplay: true,
+    //     autoplayTimeout: 2000,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         900:{
+    //             items:2
+    //         },
+    //     }
+    // })
 
     $.scrollIt({
         easing: 'linear',
